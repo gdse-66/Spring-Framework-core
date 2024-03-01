84c5c6ccea9aa5e0a37b105044a61da10901d0ae
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-03-01 14:18:58 +0530

    learn constructor resolution algorithm

--- constructor-resolution-algorithm/src/main/java/lk/ijse/gdse66/spring/AppConfig.java
+++ constructor-resolution-algorithm/src/main/java/lk/ijse/gdse66/spring/AppConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.gdse66.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -7,4 +8,19 @@ import org.springframework.stereotype.Component;
 @Configuration
 @ComponentScan
 public class AppConfig {
+
+    @Bean
+    public String myString(){
+        return "Amal";
+    }
+
+    @Bean
+    public Integer myInt(){
+        return 21;
+    }
+
+    @Bean
+    public Boolean myBoolean(){
+        return true;
+    }
 }
--- constructor-resolution-algorithm/src/main/java/lk/ijse/gdse66/spring/bean/SpringBean.java
+++ constructor-resolution-algorithm/src/main/java/lk/ijse/gdse66/spring/bean/SpringBean.java
@@ -1,10 +1,44 @@
 package lk.ijse.gdse66.spring.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
-    public SpringBean() {
+    private String name;
+    private int age;
+    private boolean isMale;
+    private float temp;
+
+    /*public SpringBean() {
         System.out.println("SpringBean() - Constructor");
+    }*/
+
+    public SpringBean(String name, int age, boolean isMale) {
+        this.name = name;
+        this.age = age;
+        this.isMale = isMale;
+        System.out.println("SpringBean(String, int, boolean)");
+        System.out.printf("name=%s, age=%s, isMale=%s\n",name,age,isMale);
+    }
+
+    /* greediest constructor (highest parameter count that Spring can satisfy) */
+    @Autowired(required = false)
+    public SpringBean(String name, int age, boolean isMale, @Nullable float temp) {
+        this.name = name;
+        this.age = age;
+        this.isMale = isMale;
+        this.temp = temp;
+        System.out.println("SpringBean(String, int, boolean, float)");
+        System.out.printf("name=%s, age=%s, isMale=%s, temp=%s\n",name,age,isMale,temp);
+    }
+
+    @Autowired(required = false)
+    public SpringBean(String name, int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("SpringBean(String, int)");
+        System.out.printf("name=%s, age=%s\n",name,age);
     }
 }
